37838ccb8eb074ac3b9d2f1b0b69724be5b62a3b
Pierre Rudloff <contact@rudloff.pro>
2016-09-07 22:38:18 +0000

    Applied fixes from StyleCI

--- shakepeers.php
+++ shakepeers.php
@@ -1,68 +1,70 @@
 <?php
 /**
- * Shakepeers Skin
+ * Shakepeers Skin.
  *
  * @shakepeers.php
  * @ingroup Skins
+ *
  * @author Mark Nightingale (http://marknightingale.net)
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
+if (!defined('MEDIAWIKI')) {
+    die('This is an extension to the MediaWiki package and cannot be run standalone.');
+}
 
-if ( ! defined( 'MEDIAWIKI' ) ) die( "This is an extension to the MediaWiki package and cannot be run standalone." );
-
-$wgExtensionCredits['skin'][] = array(
-	'path'        => __FILE__,
-	'name'        => 'Shakepeers Skin',
-	'url'         => 'http://shakepeers.org',
-	'author'      => '[http://marknightingale.net Mark Nightingale',
-	'description' => 'MediaWiki skin for Shakepeers using Bootstrap 3',
-);
+$wgExtensionCredits['skin'][] = [
+    'path'        => __FILE__,
+    'name'        => 'Shakepeers Skin',
+    'url'         => 'http://shakepeers.org',
+    'author'      => '[http://marknightingale.net Mark Nightingale',
+    'description' => 'MediaWiki skin for Shakepeers using Bootstrap 3',
+];
 
 $wgValidSkinNames['shakepeers'] = 'Shakepeers';
-$wgAutoloadClasses['SkinShakepeers'] = __DIR__ . '/Shakepeers.skin.php';
+$wgAutoloadClasses['SkinShakepeers'] = __DIR__.'/Shakepeers.skin.php';
 
 
-$skinDirParts = explode( DIRECTORY_SEPARATOR, __DIR__ );
-$skinDir = array_pop( $skinDirParts );
+$skinDirParts = explode(DIRECTORY_SEPARATOR, __DIR__);
+$skinDir = array_pop($skinDirParts);
 
-$wgMessagesDirs['shakepeers'] = __DIR__ . '/i18n';
+$wgMessagesDirs['shakepeers'] = __DIR__.'/i18n';
 
-$wgResourceModules['skins.shakepeers'] = array(
-	'styles' => array(
-		$skinDir . '/vendor/bootstrap/css/bootstrap.min.css'            => array( 'media' => 'all' ),
-		$skinDir . '/vendor/google-code-prettify/prettify.css'          => array( 'media' => 'all' ),
-		$skinDir . '/style.css'                                         => array( 'media' => 'all' ),
-		$skinDir . '/vendor/flatline/style.css'                         => array( 'media' => 'all' ),
-		$skinDir . '/vendor/yamm/yamm.css'                              => array( 'media' => 'all' ),
-        $skinDir . '/vendor/jansy/jasny-bootstrap.min.css'              => array( 'media' => 'all' ),
-		$skinDir . '/print.css'                                         => array( 'media' => 'print' ),
-		$skinDir . '/custom.css'                                        => array( 'media' => 'all' ),
-        
-        
-        
-	),
-	'scripts' => array(
-		$skinDir . '/vendor/bootstrap/js/bootstrap.min.js',
-		$skinDir . '/vendor/google-code-prettify/prettify.js',
-		$skinDir . '/js/jquery.ba-dotimeout.min.js',
-		$skinDir . '/vendor/jansy/jasny-bootstrap.min.js',
-		$skinDir . '/js/behavior.js',
-        $skinDir . '/myscripts.js'
-	),
-	'dependencies' => array(
-		'jquery',
-		'jquery.mwExtension',
-		'jquery.client',
-		'jquery.cookie',
-	),
-	'remoteBasePath' => &$GLOBALS['wgStylePath'],
-	'localBasePath'  => &$GLOBALS['wgStyleDirectory'],
-);
+$wgResourceModules['skins.shakepeers'] = [
+    'styles' => [
+        $skinDir.'/vendor/bootstrap/css/bootstrap.min.css'            => ['media' => 'all'],
+        $skinDir.'/vendor/google-code-prettify/prettify.css'          => ['media' => 'all'],
+        $skinDir.'/style.css'                                         => ['media' => 'all'],
+        $skinDir.'/vendor/flatline/style.css'                         => ['media' => 'all'],
+        $skinDir.'/vendor/yamm/yamm.css'                              => ['media' => 'all'],
+        $skinDir.'/vendor/jansy/jasny-bootstrap.min.css'              => ['media' => 'all'],
+        $skinDir.'/print.css'                                         => ['media' => 'print'],
+        $skinDir.'/custom.css'                                        => ['media' => 'all'],
 
-if ( isset( $wgSiteJS ) ) {
-	$wgResourceModules['skins.shakepeers']['scripts'][] = $skinDir . '/' . $wgSiteJS;
+
+
+    ],
+    'scripts' => [
+        $skinDir.'/vendor/bootstrap/js/bootstrap.min.js',
+        $skinDir.'/vendor/google-code-prettify/prettify.js',
+        $skinDir.'/js/jquery.ba-dotimeout.min.js',
+        $skinDir.'/vendor/jansy/jasny-bootstrap.min.js',
+        $skinDir.'/js/behavior.js',
+        $skinDir.'/myscripts.js',
+    ],
+    'dependencies' => [
+        'jquery',
+        'jquery.mwExtension',
+        'jquery.client',
+        'jquery.cookie',
+    ],
+    'remoteBasePath' => &$GLOBALS['wgStylePath'],
+    'localBasePath'  => &$GLOBALS['wgStyleDirectory'],
+];
+
+if (isset($wgSiteJS)) {
+    $wgResourceModules['skins.shakepeers']['scripts'][] = $skinDir.'/'.$wgSiteJS;
 }//end if
 
-if ( isset( $wgSiteCSS ) ) {
-	$wgResourceModules['skins.shakepeers']['styles'][] = $skinDir . '/' . $wgSiteCSS;
+if (isset($wgSiteCSS)) {
+    $wgResourceModules['skins.shakepeers']['styles'][] = $skinDir.'/'.$wgSiteCSS;
 }//end if
